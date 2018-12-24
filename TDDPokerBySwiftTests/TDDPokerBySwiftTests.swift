2ff64c4fd70bb32698fb3dbdfac8f3d587dfc3a1
mi81ma <mi81ma@gmail.com>
2018-12-24 14:26:24 +0800

    First Test is Success

--- TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
+++ TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
@@ -11,15 +11,16 @@ import XCTest
 
 class TDDPokerBySwiftTests: XCTestCase {
 
-    class TDDPokerBySwiftTests: XCTestCase {
+    func testInitializeCard() {
 
         let card = Card(suit: .heart, rank: .three)
         XCTAssertEqual(card.suit, .heart)
-        XCTAssertEqual(card,rank, .three)
+        XCTAssertEqual(card.rank, .three)
 
 
         let card2 = Card(suit: .spade, rank: .jack)
         XCTAssertEqual(card2.suit, .spade)
         XCTAssertEqual(card2.rank, .jack)
+
     }
 }
