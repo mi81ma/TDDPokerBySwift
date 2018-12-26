3cdc36c3626d785b30e7dcc19f83034097aeb05e
mi81ma <mi81ma@gmail.com>
2018-12-26 15:13:03 +0800

    4th test success

--- TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
+++ TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
@@ -30,8 +30,14 @@ class TDDPokerBySwiftTests: XCTestCase {
 
         card1 = Card(rank: .ace, suit: .heart)
         card2 = Card(rank: .two, suit: .heart)
+        XCTAssertTrue(card1.hasSameSuit(card2))
+
+
+        card1 = Card(rank: .ace, suit: .spade)
+        card2 = Card(rank: .two, suit: .heart)
         XCTAssertFalse(card1.hasSameSuit(card2))
     }
+
 
     func testHasSameRank() {
         var card1: Card
